2d99c2489311dbc2fb51fe50450d0ab28696a40a
luca <luca.ciccioti@virgilio.it>
2018-02-18 23:47:00 +0100

    adding call function

--- app/src/main/java/luigi/casciaro/cityparty/view/AdDetailActivity.java
+++ app/src/main/java/luigi/casciaro/cityparty/view/AdDetailActivity.java
@@ -1,11 +1,19 @@
 package luigi.casciaro.cityparty.view;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import luigi.casciaro.cityparty.AppController;
 import luigi.casciaro.cityparty.R;
@@ -15,6 +23,9 @@ import luigi.casciaro.cityparty.util.MyUtil;
 public class AdDetailActivity extends BaseActivity {
 
     Ad ad;
+    ImageView imgphone;
+    private static final int MAKE_CALL_PERMISSION_REQUEST_CODE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +45,40 @@ public class AdDetailActivity extends BaseActivity {
         // listener
         findViewById(R.id.viewLocation).setOnClickListener(v -> goToMaps());
         findViewById(R.id.imageViewMap).setOnClickListener(v -> goToMaps());
+
+
+        // phone
+
+        imgphone = (ImageView) findViewById(R.id.imagePhone);
+        imgphone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("click", "phone");
+                if (checkPermission(Manifest.permission.CALL_PHONE)) {
+                    String dial = "tel:" + "111111222";
+                    startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+                } else {
+                    ActivityCompat.requestPermissions(AdDetailActivity.this, new String[]{Manifest.permission.CALL_PHONE}, MAKE_CALL_PERMISSION_REQUEST_CODE);
+
+                }
+            }
+        });
+
     }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MAKE_CALL_PERMISSION_REQUEST_CODE:
+                //String numberPhone = ad.getPhone();
+                Toast.makeText(this, "Adesso puoi effettuare la chiamata", Toast.LENGTH_SHORT).show();
+                return;
+        }
+    }
+
+    private boolean checkPermission(String permission) {
+        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED;
+    }
+
 
     void goToMaps(){
         Uri gmmIntentUri = Uri.parse("google.streetview:cbll="+ad.getLatitude()+","+ad.getLongitude());
